ee71ddba3aff159644ea35113b702aaf035c7590
debbiejansen <debbiejansen94@hotmail.com>
2025-12-06 16:59:17 +0100

    eerste card gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import './App.css'
+import {useState} from "react";
 
 function App() {
+    const [count, setCount] = useState(0)
 
   return (
     <>
         <h1>Fruitmand bezorgservice</h1>
+        <div className={"card"}>
+            <h2>🍓Aardbeien</h2>
+            <button onClick={() => setCount((count) => count - 1)}>
+            - </button>
+            {count}
+            <button onClick={() => setCount((count) => count + 1)}>
+                + </button>
+        </div>
+
+        <h2>🍌Bananen</h2>
+        <h2>🍎Appels</h2>
+        <h2>🥝Kiwi's</h2>
     </>
   )
 }
