e3a234d8c4224e1bf8d462fee012bc0ecb82518f
debbiejansen <debbiejansen94@hotmail.com>
2025-12-07 16:21:11 +0100

    form basis opgezet

--- src/App.jsx
+++ src/App.jsx
@@ -36,45 +36,171 @@ function App() {
         });
     }
 
+    const [firstNameValue, setFirstNameValue] = useState('');
+    const [lastNameValue, setLastNameValue] = useState('');
+    const [ageValue, setAgeValue] = useState('');
+    const [postcodeValue, setPostcodeValue] = useState('');
+    const [dayTimeValue, setDayTimeValue] = useState(false);
+    const [nightTimeValue, setNightTimeValue] = useState(false);
+    const [messageValue, setMessageValue] = useState('');
+    const [newsletterValue, setNewsletterValue] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(firstNameValue, lastNameValue, ageValue, postcodeValue, dayTimeValue, nightTimeValue, messageValue, newsletterValue);
+    }
+
     return (
         <>
             <header>
                 <h1>Fruitmand bezorgservice</h1>
                 <div className="container">
 
-                <FruitCounter
-                label="Aardbeien"
-                count={counts.strawberry}
-                onIncrease={function () { increase("strawberry");}}
-                onDecrease={function () { decrease("strawberry");}}
-                />
+                    <FruitCounter
+                        label="Aardbeien"
+                        count={counts.strawberry}
+                        onIncrease={function () {
+                            increase("strawberry");
+                        }}
+                        onDecrease={function () {
+                            decrease("strawberry");
+                        }}
+                    />
 
-                <FruitCounter
-                    label="Bananen"
-                    count={counts.banana}
-                    onIncrease={function () { increase("banana");}}
-                    onDecrease={function () { decrease("banana");}}
-                />
+                    <FruitCounter
+                        label="Bananen"
+                        count={counts.banana}
+                        onIncrease={function () {
+                            increase("banana");
+                        }}
+                        onDecrease={function () {
+                            decrease("banana");
+                        }}
+                    />
 
-                <FruitCounter
-                    label="Appels"
-                    count={counts.apple}
-                    onIncrease={function () { increase("apple");}}
-                    onDecrease={function () { decrease("apple");}}
-                />
+                    <FruitCounter
+                        label="Appels"
+                        count={counts.apple}
+                        onIncrease={function () {
+                            increase("apple");
+                        }}
+                        onDecrease={function () {
+                            decrease("apple");
+                        }}
+                    />
 
-                <FruitCounter
-                    label="Kiwi's"
-                    count={counts.kiwi}
-                    onIncrease={function () { increase("kiwi");}}
-                    onDecrease={function () { decrease("kiwi");}}
-                />
+                    <FruitCounter
+                        label="Kiwi's"
+                        count={counts.kiwi}
+                        onIncrease={function () {
+                            increase("kiwi");
+                        }}
+                        onDecrease={function () {
+                            decrease("kiwi");
+                        }}
+                    />
 
-                <button onClick={resetAll} className="reset-button">
-                    Reset
-                </button>
+                    <button onClick={resetAll} className="reset-button">
+                        Reset
+                    </button>
                 </div>
             </header>
+
+            <form onSubmit={handleSubmit} className="form">
+
+                    <legend>Gegevens</legend>
+                    <label htmlFor="first-name-field">
+                        Voornaam
+                        <input
+                            type="text"
+                            value={firstNameValue}
+                            onChange={(e) => setFirstNameValue(e.target.value)}
+                            id="name-field"
+                            name="name"
+                        />
+                    </label>
+                <label htmlFor="last-name-field">
+                    Achternaam
+                    <input
+                        type="text"
+                        value={lastNameValue}
+                        onChange={(e) => setLastNameValue(e.target.value)}
+                        id="name-field"
+                        name="name"
+                    />
+                </label>
+                    <label htmlFor="age-field">
+                        Leeftijd:
+                        <input
+                            type="number"
+                            value={ageValue}
+                            onChange={(e) => setAgeValue(parseInt(e.target.value))}
+                            id="age-field"
+                            name="age"
+                            placeholder="0"
+                        />
+                    </label>
+                <label htmlFor="postcode-field">
+                    Postcode
+                    <input
+                        type="text"
+                        value={postcodeValue}
+                        onChange={(e) => setPostcodeValue(e.target.value)}
+                        id="name-field"
+                        name="name"
+                    />
+                </label>
+
+
+                <fieldset>
+                    <legend>Bezorgfrequentie</legend>
+                <label htmlFor="form-day-time" className="checkbox-label">
+                    <input
+                        type="radio"
+                        id="form-day-time"
+                        name="day-time"
+                        checked={dayTimeValue}
+                        onChange={() => setDayTimeValue(!dayTimeValue)}
+                    />
+                    Overdag
+                    <input
+                        type="radio"
+                        id="form-night-time"
+                        name="night-time"
+                        checked={nightTimeValue}
+                        onChange={() => setNightTimeValue(!nightTimeValue)}
+                    />
+                    Savonds
+                </label>
+
+                    <label htmlFor="message-field">
+                    </label>
+                    <textarea
+                        id="message-field"
+                        name="message"
+                        rows="5"
+                        cols="50"
+                        placeholder="Opmerkingen"
+                        value={messageValue}
+                        onChange={(e) => setMessageValue(e.target.value)}
+                    />
+                </fieldset>
+
+                    <label htmlFor="form-newsletter" className="checkbox-label">
+                        <input
+                            type="checkbox"
+                            id="form-newsletter"
+                            name="newsletter"
+                            checked={newsletterValue}
+                            onChange={() => setNewsletterValue(!newsletterValue)}
+                        />
+                        Ik ga akkoord met de voorwaarden
+                    </label>
+
+                    <button type="submit">Verzenden</button>
+
+
+            </form>
         </>
     )
 }
